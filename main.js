b9857cd070e60a6d790e12da6de1e0bc31608aa6
Truman Tang <ttang8@mail.sfsu.edu>
2021-05-03 10:21:09 -0700

    completed

--- main.js
+++ main.js
@@ -1,23 +1,17 @@
-var testArray = [10,300,5,6,5123]; //Test array
-var stringArray = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]; //Array for each number count
-
+//Array for each number count
+var stringArray = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]; 
 //loop to assign each value in testArray 
-for(var i = 0; i < testArray.length; i++){
-    num = testArray[i]; // assign num to be the value of each value in testArray
+for(var i = 2; i < process.argv.length; i++)
+{
+    var num = process.argv[i]; // assign num to be the value of each value in testArray
     var str = ""; // initialize var str
-
-    while(num > 0){
-        var mod = (num % 10); //gets place to parse
+    while(num > 0) 
+    {
+        var mod = num%10; //gets place to parse
         str = stringArray[mod]+str; //gets the word form of parsed value and update it for however many digit it has
         num = Math.floor((num/10)); //gets the digit that code is working on
     }
-
-    testArray[i] = str+','; 
-
+    if(i+1 == process.argv.length) {process.argv[i] = str;} //gets rid of comma at the last argument
+    else {process.argv[i] = str+',';}
+    process.stdout.write(process.argv[i]);
 }
-console.log(...testArray);
-
-/*
-TODO: GET RID OF LAST COMMA
-      ALLOW FOR USER INPUT TO REPLACE TES TARRAY
-*/
