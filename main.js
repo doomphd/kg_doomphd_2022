714c621b8e9588240a4e2da9975f2973c3fab279
Truman Tang <ttang8@mail.sfsu.edu>
2021-05-03 09:48:12 -0700

    fixed comma problem and got the output to only print 1 line

--- main.js
+++ main.js
@@ -1,28 +1,24 @@
-// allow for command line arguments
-process.argv.forEach(function (val, index, testArray) {
-  //console.log(index + ': ' + val);
-  var stringArray = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]; //Array for each number count
+//Array for each number count
+var stringArray = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]; 
 
 //loop to assign each value in testArray 
-for(var i = 2; i < testArray.length; i++){
-    num = testArray[i]; // assign num to be the value of each value in testArray
+for(var i = 2; i < process.argv.length; i++){
+    var num = process.argv[i]; // assign num to be the value of each value in testArray
     var str = ""; // initialize var str
 
     while(num > 0){
-        var mod = (num % 10); //gets place to parse
+        var mod = num%10; //gets place to parse
         str = stringArray[mod]+str; //gets the word form of parsed value and update it for however many digit it has
         num = Math.floor((num/10)); //gets the digit that code is working on
     }
-
-    testArray[i] = str+','; 
-
+    if(i+1 == process.argv.length) {process.argv[i] = str;}
+    else {process.argv[i] = str+',';}
 }
-console.log(...testArray);
-});
+console.log(...process.argv);
 
 
 
 /*
-TODO: GET RID OF LAST COMMA
-      get rid of extra arrays prints
+TODO: 
+      get rid of excess output print
 */
